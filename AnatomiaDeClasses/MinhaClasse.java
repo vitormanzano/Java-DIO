0de30a2b3c53ad5d6a59a643251aa6a93e940bbf
Vitor Manzano Villela Dias <vimanzanovillela@gmail.com>
2025-01-20 09:48:34 -0300

    ANatomia de classes

---
+++ AnatomiaDeClasses/MinhaClasse.java
@@ -0,0 +1,14 @@
+package AnatomiaDeClasses;
+
+public class MinhaClasse {
+    public static void main(String[] args) {
+        String primeiroNome = "Vitor";
+        String SegundoNome = "Manzano";
+
+        String nomeCompleto = nomeCompleto(primeiroNome, SegundoNome);
+        System.out.println(nomeCompleto);
+    }
+    public static String nomeCompleto (String primeiroNome, String segundoNome) {
+        return primeiroNome.concat(" ").concat(segundoNome);
+    }
+}
