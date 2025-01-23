dff9bd5d794b4bc029c75f5bb5c038416b3d69bc
Vitor Manzano Villela Dias <vimanzanovillela@gmail.com>
2025-01-23 17:09:34 -0300

    DEsafio 01 POO JAVA

---
+++ Desafio01POO/src/Telefone/Iphone.java
@@ -0,0 +1,53 @@
+package Telefone;
+
+import Musical.ReprodutorMusical;
+import NavegadorInternet.NavegadorInternet;
+import Telefonico.AparelhoTelefonico;
+
+public class Iphone implements ReprodutorMusical, NavegadorInternet, AparelhoTelefonico {
+
+    @Override
+    public void tocar() {
+
+    }
+
+    @Override
+    public void pausar() {
+
+    }
+
+    @Override
+    public void selecionarMusica(String musica) {
+
+    }
+
+    @Override
+    public void exibirPagina(String url) {
+
+    }
+
+    @Override
+    public void adicionarNovaAba() {
+
+    }
+
+    @Override
+    public void atualizarPagina() {
+
+    }
+
+    @Override
+    public void ligar(String numero) {
+
+    }
+
+    @Override
+    public void atender() {
+
+    }
+
+    @Override
+    public void iniciarCorreioVoz() {
+
+    }
+}
