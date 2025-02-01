1b0a6b7b9de1bdf0e7fb67ca393544514c173e18
Vitor Manzano Villela Dias <vimanzanovillela@gmail.com>
2025-02-01 13:25:43 -0300

    Lista circular

---
+++ EstruturaDeDados/src/ListasCirculares/Main.java
@@ -0,0 +1,20 @@
+package ListasCirculares;
+
+public class Main {
+    public static void main(String[] args) {
+        Lista<String> minhaLista = new Lista<String>();
+
+        minhaLista.add("C0");
+
+
+        minhaLista.add("C1");
+        minhaLista.add("C2");
+        minhaLista.add("C3");
+
+        System.out.println(minhaLista);
+
+        System.out.println(minhaLista.get(4 ));
+
+    }
+
+}
